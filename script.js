0ff96ed04cbe16744f411230ff74674c0351fdff
Anwarul07 <mdanwarul064@gmail.com>
2024-06-02 23:40:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
 let productImg = document.getElementById("product-img");
 let btn = document.getElementsByClassName("btn");
 
+
+btn[0].addEventListener("click", () => {
+    productImg.src = "first.jpg";
+
+    for (bt of btn) {
+        bt.classList.remove("active")
+    }
+    btn[0].classList.add("active");
+    console.log("First image ")
+});
+
+btn[1].addEventListener("click", () => {
+    productImg.src = "second.jpg";
+
+    for (bt of btn) {
+        bt.classList.remove("active")
+    }
+   btn[1].classList.add("active");
+    console.log("second image ")
+});
+
+
+btn[2].addEventListener("click", () => {
+    productImg.src = "third.jpg";
+
+
+    for (bt of btn) {
+        bt.classList.remove("active")
+    }
+    btn[2].classList.add("active");
+    // this.classList.add("active");
+    console.log("third image ")
+});
